3ca06f31165a77dceeaab30cc97c3aba5adbea67
ashan1979 <indrajee79@gmail.com>
2026-01-12 19:20:17 +0530

    added normalization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pydub import AudioSegment,
+from pydub import AudioSegment, effects
 def snip_audio(input_file, start_sec, end_sec, output_file):
     # pydub works in milliseconds
     start_ms = start_sec * 1000
@@ -10,8 +10,15 @@ def snip_audio(input_file, start_sec, end_sec, output_file):
     print(f"Snipping from {start_sec}s to {end_sec}s...")
     snip = audio[start_ms:end_ms]
 
-    snip.export(output_file, format="wav")
-    print(f"Saving to {output_file}")
+    # --- NEW: Normalization ---
+    #This brings the peak volume to 0 dB (or very close to it)
+    print(f"Normalizing audio levels...")
+    normalized_snip = effects.normalize(snip)
+    # --------------------------
+
+    normalized_snip.export(output_file, format="wav")
+    print(f"Saved normalized snip to {output_file}")
+
 
 # Example usage (uncomment and change 'my_song.wav' to a real file you have)
 # snip_audio("my_song.wav", 10, 20, "snip_output.wav")
